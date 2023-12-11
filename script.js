66db0cb3b940e871078aba1a9c25ff53af52f1cb
jozef-thomas <52873243+jozef-thomas@users.noreply.github.com>
2023-12-11 12:49:29 +0530

    Add files via upload
    My first Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// console.log("hello")
+// console.log("hai")
+// for(i=1;i<=10;i++){
+//     if(i%2==0)
+//     console.log(i+" Even")
+//     else if(i%2!=0)
+//     console.log(i+" Odd")
+// }
+// console.error("Danger")
+
+// {
+//     let a = 10
+//     console.log(a)
+// }
+// console.log(a)
+
+
+// {
+//     var a = 10
+//     console.log(a)
+// }
+// var a = 12
+// console.log(a)
+
+// {
+//     const b = 10
+//     console.log(b)
+//     b = 20
+// }
+// console.log(b)
+
+
+
+// Arrays
+
+const cars = ["BMW","Benz","Audi"]
+const carnames = ["X5","S-Class","A8"]
+console.log(cars[0])
+console.log(cars[1])
+console.log(cars[2])
+
+// spread operator
+const carcombined = [...cars,...carnames]
+console.log(carcombined)
+
+// Objects
+
+// const person = {
+//     first_name:"Joseph",
+//     age:"25",
+//     place:"ktm"
+// }
+
+// console.log(person.first_name)
